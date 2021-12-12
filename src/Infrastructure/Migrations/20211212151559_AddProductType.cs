de8e5403f91591c78a7d73d3c267519676bde511
a.osadchii <roflandcom@gmail.com>
2021-12-12 18:17:16 +0300

    add product types

---
+++ src/Infrastructure/Migrations/20211212151559_AddProductType.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddProductType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductType",
+                schema: "dbo",
+                table: "Product",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                schema: "dbo",
+                table: "Product");
+        }
+    }
+}

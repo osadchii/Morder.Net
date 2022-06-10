7c36dc4c92a2d733d704b2ae40c8bf35e7c8f197
Anton Osadchii <roflandcom@gmail.com>
2022-06-10 13:52:01 +0300

    json console log

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -57,7 +57,11 @@ public class Program
         builder.Services.AddLogging(logBuilder =>
         {
             logBuilder.AddConfiguration(builder.Configuration);
+#if DEBUG
+            logBuilder.AddConsole();
+#else
             logBuilder.AddJsonConsole();
+#endif
             logBuilder.AddFilter("Microsoft.EntityFrameworkCore.Infrastructure", LogLevel.Warning);
             logBuilder.AddFilter("System.Net.Http.HttpClient.tgwebhook.ClientHandler", LogLevel.Warning);
             logBuilder.AddFilter("System.Net.Http.HttpClient.tgwebhook.LogicalHandler", LogLevel.Warning);

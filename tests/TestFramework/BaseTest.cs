fae77ce4777de96017ff0b3bcc7d944f31f07388
Anton Osadchii <roflandcom@gmail.com>
2022-06-30 18:25:38 +0300

    fix tests

--- tests/TestFramework/BaseTest.cs
+++ tests/TestFramework/BaseTest.cs
@@ -1,5 +1,4 @@
 using Api;
-using TestFramework.Users;
 using Xunit;
 
 namespace TestFramework;
@@ -11,17 +10,5 @@ public abstract class BaseTest : IClassFixture<MorderWebApplicationFactory<Progr
     protected BaseTest(MorderWebApplicationFactory<Program> factory)
     {
         Client = factory.CreateClient();
-        var userService = new UserService(Client);
-
-        var user = new User()
-        {
-            Name = Guid.NewGuid().ToString(),
-            Password = Guid.NewGuid().ToString()
-        };
-        
-        userService.RegisterUser(user).GetAwaiter().GetResult();
-        ServiceActionResult<ApiResult<TokenDto>> tokenResult = userService.GetToken(user).GetAwaiter().GetResult();
-
-        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {tokenResult.Entity.Value.Token}");
     }
 }

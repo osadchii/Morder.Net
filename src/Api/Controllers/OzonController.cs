351d614cadb9dce99ac22b8c55d1401dbca794a5
Anton Osadchii <roflandcom@gmail.com>
2022-07-06 08:23:14 +0300

    get ozon by id

--- src/Api/Controllers/OzonController.cs
+++ src/Api/Controllers/OzonController.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Infrastructure.Common;
 using Infrastructure.MediatR.Marketplaces.Ozon.Commands;
+using Infrastructure.MediatR.Marketplaces.Ozon.Queries;
 using Infrastructure.Models.Marketplaces.Ozon;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -33,6 +34,17 @@ public class OzonController : ControllerBase
     {
         command.Id = id;
         OzonDto result = await _mediator.Send(command);
+        return result.AsResult();
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<Result> GetOzonById([Required] int id)
+    {
+        OzonDto result = await _mediator.Send(new GetOzonByIdRequest()
+        {
+            Id = id
+        });
         return result.AsResult();
     }
 }
---
+++ src/Infrastructure/MediatR/Marketplaces/Ozon/Queries/GetOzonByIdHandler.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using AutoMapper;
+using Infrastructure.Models.Marketplaces;
+using Infrastructure.Models.Marketplaces.Ozon;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.MediatR.Marketplaces.Ozon.Queries;
+
+public class GetOzonByIdRequest : IRequest<OzonDto>
+{
+    public int Id { get; set; }
+}
+
+public class GetOzonByIdHandler : IRequestHandler<GetOzonByIdRequest, OzonDto>
+{
+    private readonly MContext _context;
+    private readonly IMapper _mapper;
+
+    public GetOzonByIdHandler(MContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public async Task<OzonDto> Handle(GetOzonByIdRequest request, CancellationToken cancellationToken)
+    {
+        OzonDto? result = await _context.Marketplaces
+            .AsNoTracking()
+            .Include(m => m.Warehouse)
+            .Include(m => m.PriceType)
+            .Where(m => m.Id == request.Id && m.Type == MarketplaceType.Ozon)
+            .Select(m => _mapper.Map<OzonDto>(m))
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (result is not null) return result;
+        
+        var message = $"Ozon with {request.Id} id not found";
+        throw new HttpRequestException(message, null, HttpStatusCode.NotFound);
+    }
+}

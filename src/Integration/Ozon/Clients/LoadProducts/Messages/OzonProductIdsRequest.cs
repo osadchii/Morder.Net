5f97cbc8c241e1e29422b733c72e25bb3cec5c04
anton.osadchii <anton@osadchii.me>
2025-02-20 17:19:58 +0200

    ozon product list updates

--- src/Integration/Ozon/Clients/LoadProducts/Messages/OzonProductIdsRequest.cs
+++ src/Integration/Ozon/Clients/LoadProducts/Messages/OzonProductIdsRequest.cs
@@ -7,4 +7,11 @@ public record OzonProductIdsRequest
     [JsonProperty("last_id")] public string LastId { get; set; } = null!;
 
     [JsonProperty("limit")] public int Limit { get; set; }
+    [JsonProperty("filter")] public RequestFilter Filter { get; set; } = new();
+    
+    public class RequestFilter
+    {
+        [JsonProperty("visibility")]
+        public string Visibility { get; set; } = "ALL";
+    }
 }
